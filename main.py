370414aa50b9315556832e9da3a44401b662cf61
Illuamon <nsleskova@protonmail.com>
2025-11-17 22:45:44 +0100

    costume prompts, error handling

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
-    load_dotenv()
-    apikey = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=apikey)
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    prompt = sys.argv
+    
+    if len(prompt) <= 1:
+        sys.exit("Error: no prompt")
+    
+    else:
+        load_dotenv()
+        apikey = os.environ.get("GEMINI_API_KEY")
+        client = genai.Client(api_key=apikey)
+        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt[1])
+        print(response.text)
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
