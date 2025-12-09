d3e1f61eb69e16ab754932dd3e8a7fefae1f5624
Illuamon <nsleskova@protonmail.com>
2025-12-09 21:00:15 +0100

    call_function working

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-from call_functions import available_functions
+from call_function import available_functions, call_function
 from prompts import system_prompt
 
 def main():
     raw_args = sys.argv
-    text_prompt = raw_args[1]
+    verbose = False
 
     if len(raw_args) <= 1:
         sys.exit("Error: no prompt")
+    
+    if "--verbose" in raw_args:
+        verbose = True
 
+    text_prompt = raw_args[1]
     messages = [types.Content(role="user", parts=[types.Part(text=text_prompt)]),]    
 
     load_dotenv()
@@ -21,7 +25,7 @@ def main():
     client = genai.Client(api_key=apikey)
 
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", 
+        model="gemini-2.5-flash", 
         contents=messages,
         config=types.GenerateContentConfig(
                 tools=[available_functions], system_instruction=system_prompt
@@ -29,14 +33,23 @@ def main():
     )
 
     function_call_part = response.function_calls
+    function_response_list = []
 
     if not function_call_part:
         print(response.text)
     else:
         for f in function_call_part:
-            print(f"Calling function: {f.name}({f.args})")
-    
-    if "--verbose" in raw_args:
+            function_call_result = call_function(f, verbose)
+
+            if not function_call_result.parts[0].function_response.response:
+                raise Exception("no function_response.response")
+            else:
+                function_response_list.append(function_call_result.parts[0])
+            
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+
+    if verbose:
         print(f"User prompt: {text_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
